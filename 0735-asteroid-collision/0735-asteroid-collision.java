15972fd53fc24ce2441c287f10e2289e29e092b7
Ian Cheng <91156531+chengtc-dev@users.noreply.github.com>
2024-12-09 20:46:17 +0800

    Time: 2 ms (98.42%), Space: 45.3 MB (61.39%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                while (!stack.isEmpty() &&
+                        stack.peek() > 0 &&
+                        stack.peek() < Math.abs(asteroid)) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                }
+
+                if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] result = new int[stack.size()];
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
