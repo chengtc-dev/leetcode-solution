a847cf661fa6df6abbfdd64feab06fc7c2d85f65
Ian Cheng <91156531+chengtc-dev@users.noreply.github.com>
2025-01-28 12:43:22 +0800

    Time: 1581 ms (49.83%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,12 +1,16 @@
 /* Write your PL/SQL query statement below */
-select st.student_id, st.student_name, su.subject_name, 
+select st.student_id,
+       st.student_name, 
+       su.subject_name, 
        count(ex.subject_name) as attended_exams
   from Students st
   join Subjects su 
-       on su.subject_name is not null
+       on 1 = 1
   left join Examinations ex 
        on st.student_id = ex.student_id 
        and su.subject_name = ex.subject_name
- group by st.student_id, st.student_name,
-          su.subject_name, ex.subject_name
- order by st.student_id, su.subject_name;
+ group by st.student_id,
+          st.student_name,
+          su.subject_name
+ order by st.student_id,
+          su.subject_name;
